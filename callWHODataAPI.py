5e541f120d1484f4a3fe01c47b41e4f48aa2278c
Rafal Sladek <rafal.sladek@gmail.com>
2016-04-04 10:16:12 +0200

    fixed typo

--- callWHODataAPI.py
+++ callWHODataAPI.py
@@ -4,7 +4,7 @@ import urllib.request
 class WHODataAPI:
     def __init__(self):
         self.baseUrl = "http://apps.who.int/gho/athena/api/"
-        self.format = "foramt=json"
+        self.format = "format=json"
         self.encoding = "utf8"
 
     def get_who_dimensions(self):
